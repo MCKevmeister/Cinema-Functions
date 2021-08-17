21ad6edb9ecf79db4c41b6b1002f3e7c1d580916
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-08-18 06:10:37 +1200

    cs project files

---
+++ Cinema-Functions/Cinema-Functions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cinema_Functions
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
