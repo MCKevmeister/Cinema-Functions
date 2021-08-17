7def8ec0c616828b87b499af14e8ba91c1f83b4d
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-08-18 10:58:39 +1200

    Add Adult after 5 tests

--- Cinema-Functions/Cinema-Tests/Tests.cs
+++ Cinema-Functions/Cinema-Tests/Tests.cs
@@ -8,18 +8,18 @@ namespace Cinema_Tests
     [TestFixture]
     public class AdultBefore5
     {
-        [TestCase]
+        [TestCase(Author = "Mark Christison")]
         public void When_OneAdultMonday1pm_Expect_Cost1450()
         {
             TicketPriceController ticketPriceController = new();
-            var adultTicketCost = ticketPriceController.Adult_Before_5(1, "adult", "monday", 1.00m);
+            var adultTicketCost = ticketPriceController.Adult_Before_5(1, "adult", "monday", 2.00m);
             Assert.AreEqual(adultTicketCost, 14.5);
         }
         [TestCase]
         public void When_1AdultSaturday2pm_Expect_Cost1450()
         {
             TicketPriceController ticketPriceController = new();
-            var adultTicketCost = ticketPriceController.Adult_Before_5(1, "adult", "monday", 2.00m);
+            var adultTicketCost = ticketPriceController.Adult_Before_5(1, "adult", "saturday", 2.00m);
             Assert.AreEqual(adultTicketCost, 14.5);
         }
         [TestCase]
@@ -57,13 +57,13 @@ namespace Cinema_Tests
             var adultTicketCost = ticketPriceController.Adult_Before_5(1, "senior", "wednesday", 3.00m);
             Assert.AreEqual(adultTicketCost, -1);
         }
-        // [TestCase]
-        // public void When_1AdultWednesday10am_Expect_Cost1450()
-        // {
-        //     TicketPriceController ticketPriceController = new();
-        //     var adultTicketCost = ticketPriceController.Adult_Before_5(1, "adult", "wednesday", 10.00m);
-        //     Assert.AreEqual(adultTicketCost, 14.5);
-        // }
+        [TestCase(Ignore = "Possible identified bug in code")]
+        public void When_1AdultWednesday10am_Expect_Cost1450()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_Before_5(1, "adult", "wednesday", 10.00m);
+            Assert.AreEqual(adultTicketCost, 14.5);
+        }
         [TestCase]
         public void When_1FamilyWednesday3_Expect_negative1()
         {
@@ -72,4 +72,52 @@ namespace Cinema_Tests
             Assert.AreEqual(adultTicketCost, -1);
         }
     }
+
+    [TestFixture]
+    public class AdultAfter5
+    {
+        [TestCase]
+        public void When_1AdultWednesday6pm_Expect_Cost1750()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_After_5(1, "adult", "wednesday", 6.00m);
+            Assert.AreEqual(adultTicketCost, 17.5);
+        }
+        [TestCase]
+        public void When_2AdultThursday745_Expect_Cost35()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_After_5(2, "adult", "thursday", 7.45m);
+            Assert.AreEqual(adultTicketCost, 35);
+        }
+        [TestCase]
+        public void When_1AdultSaturday800_Expect_Cost1750()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_After_5(1, "adult", "saturday", 8.00m);
+            Assert.AreEqual(adultTicketCost, 17.50);
+        }
+        [TestCase]
+        public void When_1StudentWednesday_Expect_Negative1()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_After_5(1, "student", "wednesday", 8.00m);
+            Assert.AreEqual(adultTicketCost, -1);
+        }
+        [TestCase]
+        public void When_1FamilyWednesday_Expect_Negative1()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_After_5(1, "family", "wednesday", 8.00m);
+            Assert.AreEqual(adultTicketCost, -1);
+        }
+        [TestCase]
+        public void When_1SeniorWednesday_Expect_Negative1()
+        {
+            TicketPriceController ticketPriceController = new();
+            var adultTicketCost = ticketPriceController.Adult_After_5(1, "senior", "wednesday", 8.00m);
+            Assert.AreEqual(adultTicketCost, -1);
+        }
+    }
+ 
 }
